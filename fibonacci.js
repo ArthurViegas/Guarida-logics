8e1f07173fe7a3ffcc9d2c3737ef7386483ac0e4
Arthur Viegas <viegas.arthur2013@hotmail.com>
2024-01-26 23:45:33 -0300

    refact: separate function that generate fibonacci and add comments explaining the function logics

--- fibonacci.js
+++ fibonacci.js
@@ -9,25 +9,32 @@ const rl = readline.createInterface({
 });
 
 rl.question('Insira um numero inteiro e positivo: ', (n) => {
-    let n1 = 0;
-    let n2 = 1;
-    let next = 0;
-    let fibonacci = [];
     const value = Number(n);
 
     if(!Number.isNaN(value))
     {
-      for (let i = 1; i <= value; i++) {
-          fibonacci.push(n1);
-          next = n1 + n2;
-          n1 = n2;
-          n2 = next;
-      }
-
-      console.log(fibonacci);
+      console.log(generateFibonacci(value));
     } else {
       console.log('Por favor insira um numero inteiro e positivo');
     }
     rl.close();
 });
 
+function generateFibonacci(value) {
+  let n1 = 0;
+  let n2 = 1;
+  let next = 0;
+  let fibonacci = [];
+
+  // loop a partir do numero informado para gerar a sequencia.
+  for (let i = 1; i <= value; i++) {
+    fibonacci.push(n1);
+    next = n1 + n2; // calcula o próximo numero da sequencia, adicionando os ultimos 2.
+
+    // prepara as variaveis com valores atualizados para a proxima sequencia.
+    n1 = n2;
+    n2 = next;
+  }
+
+  return fibonacci;
+}
